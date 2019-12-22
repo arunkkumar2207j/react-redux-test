a74cd0b4522f4b03753c908e0c0f8e21bcff09b2
Arun Kamble <arunkkumar2207j@gmail.com>
2019-12-22 16:18:45 +0530

    add files

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.scss'
+
+const Header = (props) => {
+    return (
+        <header>
+            <div className="wrap">
+                SimpleTuts
+            </div>
+        </header>
+    )
+}
+
+export default Header;
